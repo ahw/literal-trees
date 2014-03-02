1bf9fce8e0f52c89f7feba0939f7fc6293cef236
ahw <andrewhallagan@gmail.com>
2014-03-01 23:24:32 -0800

    Made most things Normally random.

---
+++ box-muller.js
@@ -0,0 +1,35 @@
+var Normal = function() {
+    var phase = 0
+    var v2, s
+    var polar = function() {
+      var u1, u2, v1, result
+      if (phase == 1) {
+        result = v2 * Math.sqrt(-2 * Math.log(s) / s)
+      }
+      else {
+        do {
+          u1 = Math.random()
+          u2 = Math.random()
+          v1 = 2 * u1 - 1
+          v2 = 2 * u2 - 1
+          s = v1 * v1 + v2 * v2
+        } while (s>=1 || s==0)
+        result = v1 * Math.sqrt(-2 * Math.log(s) / s)
+      }
+      phase = 1 - phase
+      return result
+    };
+
+
+    var basic = function() {
+      var u = Math.random()
+      var v = Math.random()
+      return Math.sqrt(-2*Math.log(u)) * Math.cos(2*Math.PI*v)
+    };
+
+
+    return {
+        sample: polar,
+        basicSample: basic
+    };
+};
--- normal.js
+++ normal.js
@@ -1,13 +1,12 @@
 var PAPER_WIDTH = document.getElementById("paper").offsetWidth;
 var PAPER_HEIGHT = window.innerHeight - 16; // document.getElementById("paper").offsetHeight;
-var normal = Normal();
 
 var paper = Raphael("paper", PAPER_WIDTH, PAPER_HEIGHT);
 
-var path = function () {
+var path = function() {
     var previousArgWasNumber = false;
     var pathString = "";
-    Array.prototype.slice.call(arguments, 0).forEach(function (arg, index) {
+    Array.prototype.slice.call(arguments, 0).forEach(function(arg, index) {
         if (typeof arg === 'number' && previousArgWasNumber) {
             pathString += ","; // Assume all numbers are comma separated
         }
@@ -21,33 +20,30 @@ var path = function () {
 
 var transform = path; // Alias to path. Both are just conveniences.
 
-var branch = function (args) {
+var branch = function(args) {
     var x = args.x;
     var y = args.y;
     var levels = args.levels;
-    // var angleRange = args.angleRange;
-    var angleRange = normal.sample() * 5 + 65;
+    var angleRange = args.angleRange;
     var limbAngle = args.limbAngle;
-    //    console.log('Starting branch at ' + [x, y].join(','));
+    console.log('Starting branch at ' + [x, y].join(','));
 
     if (levels === 0) {
         return;
     }
 
-    var numBranches = Math.floor(normal.sample() * 0.5 + 4);
-    //    console.log("NUM BRANCHES", numBranches);
+    var numBranches = 4; // Math.floor(Math.random() * 4); // TODO: Normal distribution should be used here.
     for (var i = 0; i < numBranches; i++) {
         var relativeAngle = -(angleRange / 2) + i * angleRange / (numBranches - 1); // Math.floor(Math.random() * angleRange);
-        relativeAngle = normal.sample() * 5 + relativeAngle;
-        var length = Math.max(0, normal.sample() * 10 + 15 * levels); // 40; // Math.random() * 40; // TODO: Normal distribution should be used here.
-        console.log('Length at depth ' + levels + ' = ' + length);
+        var length = 40; // Math.random() * 40; // TODO: Normal distribution should be used here.
+
         var b = paper.path(path('M', x, y, 'h', length));
         b.transform(transform("r", limbAngle, x, y, "r", relativeAngle, x, y));
         var endpoint = {
             x: x + length * Math.cos(-1 * (limbAngle + relativeAngle) * Math.PI / 180),
             y: y - length * Math.sin(-1 * (limbAngle + relativeAngle) * Math.PI / 180)
         };
-        //        console.log('Endpoint', endpoint);
+        console.log('Endpoint', endpoint);
         // console.log('Ending branch from ' + [x, y].join(',') + ' at ' + [endpoint.x, endpoint.y].join(','));
         branch({
             x: endpoint.x,
@@ -59,8 +55,8 @@ var branch = function (args) {
     }
 };
 
-var t0 = Date.now();
-var trunk = function (args) {
+
+var trunk = function(args) {
     var x0 = args.x0;
     var y0 = args.y0;
     var x1 = args.x1;
@@ -74,28 +70,10 @@ var trunkStartY = PAPER_HEIGHT;
 var trunkEndX = PAPER_WIDTH / 2;
 var trunkEndY = PAPER_HEIGHT - (PAPER_HEIGHT * 0.3);
 
-trunk({
-    x0: trunkStartX,
-    y0: trunkStartY,
-    x1: trunkEndX,
-    y1: trunkEndY
-});
+var normal = Normal();
 
-var result = branch({
-    x: trunkEndX,
-    y: trunkEndY,
-    levels: 5,
-    angleRange: 70,
-    limbAngle: -90
-});
-
-if (result === -1) {
-    $("#paper").html("tree died.");
+var samples = "";
+for (var i = 0; i < 100; i++) {
+    samples += 300 * normal.sample() + "\n";
 }
-document.body.style.backgroundColor = "#DEF5FF";
-var t1 = Date.now();
-console.log('Rendering time (ms)', t1 - t0);
-// var s = paper.rect(100, 100, 20, 20).attr({fill: 'blue', 'stroke-width': 0});
-// var r = paper.rect(100, 100, 20, 20);
-// var t = r.transform("t100,100r30,100,100s2,2,100,100r45s1.5");
-// console.log('transform t', t);
+console.log(samples);
